ee8646c227fa1fde2c46d581ff48397abba06fe5
Brad Morris <bradmorris@Brads-Air.attlocal.net>
2023-08-14 16:37:59 -0700

    styling updates to form to force uppercase visually

--- src/components/WordleFilter.js
+++ src/components/WordleFilter.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 export default function WordleFilter(props) {
     const {change, filter, submit} = props;
 
@@ -21,18 +20,17 @@ export default function WordleFilter(props) {
                 <div>
                     <label className='posLabel' > Letters Found
                     <input
-                        value={filter.pos1}
+                        value={filter.pos1.toUpperCase()}
                         onChange={onChange}
                         name='pos1'
                         type='text'
                         className="position-input"
                         id="pos1"
-                        
                     />
                 </label>
                 <label>
                     <input
-                        value={filter.pos2}
+                        value={filter.pos2.toUpperCase()}
                         onChange={onChange}
                         name='pos2'
                         type='text'
@@ -41,7 +39,7 @@ export default function WordleFilter(props) {
                 </label>
                 <label>
                     <input
-                        value={filter.pos3}
+                        value={filter.pos3.toUpperCase()}
                         onChange={onChange}
                         name='pos3'
                         type='text'
@@ -50,7 +48,7 @@ export default function WordleFilter(props) {
                 </label>
                 <label>
                     <input
-                        value={filter.pos4}
+                        value={filter.pos4.toUpperCase()}
                         onChange={onChange}
                         name='pos4'
                         type='text'
@@ -59,89 +57,77 @@ export default function WordleFilter(props) {
                 </label>
                 <label>
                     <input
-                        value={filter.pos5}
+                        value={filter.pos5.toUpperCase()}
                         onChange={onChange}
                         name='pos5'
                         type='text'
                         className="position-input"
                     />
-                </label>
-                    
-                </div>
-
-                
+                </label>                
+                </div>                
                 <label for="include">Letters in Word</label>
                     <input
-                        value={filter.include}
+                        value={filter.include.toUpperCase()}
                         onChange={onChange}
                         name='include'
                         type='text'
                         className="include-input"
-                    />
-                
+                    />                
                 <div className="exclude">
                     <h4>Exclude Letters</h4>
                     <div className="excludeAll">
                         <label for='exclude' className="exclude-input">From Word</label>
                             <input
-                                value={filter.exclude}
+                                value={filter.exclude.toUpperCase()}
                                 onChange={onChange}
                                 name='exclude'
                                 type='text'
                                 className="exclude-input"
                             />
                     </div>
-
-
                     <div className="excludeByLetter">
                         <label for="char1Exclude">L1</label>
                             <input
-                                value={filter.char1Exclude}
+                                value={filter.char1Exclude.toUpperCase()}
                                 onChange={onChange}
                                 name='char1Exclude'
                                 type='text'
                                 className="exLetter"
                             />
-
                         <label for="char2Exclude">L2</label>
                             <input
-                                value={filter.char2Exclude}
+                                value={filter.char2Exclude.toUpperCase()}
                                 onChange={onChange}
                                 name='char2Exclude'
                                 type='text'
                                 className="exLetter"
                             />
-                        
                         <label for="char3Exclude">L3</label>   
                             <input
-                                value={filter.char3Exclude}
+                                value={filter.char3Exclude.toUpperCase()}
                                 onChange={onChange}
                                 name='char3Exclude'
                                 type='text'
                                 className="exLetter"
                             />
                     </div>
-
                     <div className="excludeByLetter">
                         <label for="char4Exclude">L4</label>
                             <input
-                                value={filter.char4Exclude}
+                                value={filter.char4Exclude.toUpperCase()}
                                 onChange={onChange}
                                 name='char4Exclude'
                                 type='text'
                                 className="exLetter"
                             />
-                        
-                
                         <label for="char5Exclude">L5</label>
                             <input
-                                value={filter.char5Exclude}
+                                value={filter.char5Exclude.toUpperCase()}
                                 onChange={onChange}
                                 name='char5Exclude'
                                 type='text'
                                 className="exLetter"
                             />
-                        
                     </div>            
                 </div>
                 <div className="button-container">
