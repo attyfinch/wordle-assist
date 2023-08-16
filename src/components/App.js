8d1b153735a1d9a015e34c676aeb7256b707960b
Brad Morris <bradmorris@Brads-Air.attlocal.net>
2023-08-15 17:09:15 -0700

    added loading spinner animation for long api calls

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import '../App.css';
 import axios from 'axios';
+import BeatLoader from "react-spinners/BeatLoader";
 
 import WordleFilter from './WordleFilter';
 import WordsList from './WordsList';
 
 const url = process.env.REACT_APP_API
-const local = process.env.REACT_APP_LOCAL
 
 const initialFilterValues = {
   pos1: "", 
@@ -24,17 +24,35 @@ const initialFilterValues = {
 };
 
 const initialWordsList = [];
-const initialWordCount = null;
+const initialWordCount = "Submit filter to see available words";
 
 function App() {
   const [filter, setFilter] = useState(initialFilterValues);
   const [wordsList, setWordsList] = useState(initialWordsList);
   const [wordCount, setWordCount] = useState(initialWordCount)
+  const [loading, setLoading] = useState(false);
+
+  function numberFormat(num) {
+    let numString = num.toString().split('');
+    let finalNum = [];
+    for (let i = numString.length-1; i >= 0; i--) {
+      console.log(numString[i])
+      if (i === 2 && numString.length === 5) {
+        finalNum.unshift(`,${numString[i]}`);
+      } else if (i === 1 && numString.length === 4) {
+        finalNum.unshift(`,${numString[i]}`);
+      } else {
+        finalNum.unshift(numString[i]);
+      }
+    }
+    return finalNum.join('')
+  }
 
   const getWords = (wordFilter) => {
     axios.post(url, wordFilter)
       .then((res) => {
-        setWordCount(res.data.wordsReturned)
+        setLoading(false)
+        setWordCount(`${numberFormat(res.data.wordsReturned)} available words`)
         setWordsList(res.data.wordlist)
       })
       .catch((err) => {
@@ -51,6 +69,8 @@ function App() {
   }
 
   const submit = () => {
+    setLoading(true)
+
     const newFilter = {
       positions: [filter.pos1, filter.pos2, filter.pos3, filter.pos4, filter.pos5],
       include: filter.include.padEnd(5, '_').split(''),
@@ -98,7 +118,8 @@ function App() {
         </div>
       </header>
         <WordleFilter change={change} filter={filter} submit={submit} />
-      <div>{!wordCount ? "Submit filter for words" : `${wordCount} available words`}</div>
+      <div>{loading === false ? wordCount : <BeatLoader color="rgba(255, 255, 255, 1)" size={18} loading={loading}/>}</div>
+      <div></div>
       <div className='wordlist'>
         {
           wordsList.map((word, idx) => {
