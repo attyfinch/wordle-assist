8dc93b756a86e73b1aa09cc74c6134914bb00559
Brad Morris <bradmorris@Brads-Air.attlocal.net>
2023-08-18 13:21:32 -0700

    copy and formatting tweaks

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function About() {
-
     return (
         <>
         <div className="about-page">
@@ -9,7 +8,7 @@ export default function About() {
                 WordleAssist is a simple app that helps solve Wordle games. Filling out and submitting the filter will reveal all possible correct answers that remain.
             </div>
             <div className="about-block">
-                Here's a brief explanation of how each filter field works.
+                Below is a brief explanation of how each filter field works.
             </div>
             <div className="about-block">
                 <strong>Letters Found</strong>
@@ -25,7 +24,13 @@ export default function About() {
             </div>
             <div className="about-block">
                 <strong>Exclude Letters - L1, L2, L3, L4 and L5</strong>
-                <div>Each field corresponds to a position in the word. For example, L1 refers to position 1, L2 to position 2 and so on. Place letters here that you know the word contains, but do not know the exact position of. As an example, imagine you know the word contains the letter A, but you know that it is not L1 or L2. You would put the letter A in these two fields. All words where letter A appears at these positions will be eliminated from results returned.</div>
+                <div>
+                    Each field corresponds to a position in the word. For example, L1 refers to position 1, L2 to position 2, and so on. 
+                </div>
+                <br></br>
+                <div>
+                Place letters here that you know the word contains, but do not know the exact position of. As an example, imagine you know the word contains the letter A, but you know that it is not L1 or L2. You would put the letter A in these two fields. All words where letter A appears at these positions will be eliminated from the results returned.
+                </div>
             </div>
             <div className="about-block">
                 <strong>Resetting the Filter</strong>
